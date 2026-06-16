002fd883bed8811a754b06c2c9df949504198939
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-06-16 19:56:02 +0530

    formatting byte utility function's file's index renamed

--- utils/formatter.py
+++ utils/formatter.py
@@ -1,6 +1,8 @@
-def format_bytes(num_bytes: float) -> float:
+def format_bytes(num_bytes: float) -> str:
     for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
         if num_bytes < 1024:
             return f"{num_bytes:.2f} {unit}"
 
         num_bytes /= 1024
+
+    return f"{num_bytes:.2f} PB"
